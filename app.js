a5303caa9510645cb3fce24b1a10154e172428ef
neonilakunik <n.kunyk-2025@kpi.ua>
2021-03-10 11:25:19 +0200

    Check

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ module.exports = async function (fastify, opts) {
     options: { opts }
   });
 
+  console.log('it work!');
+
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
