95f815b18abd2ce5d6f195f3253f2681b509ada9
LitusAlina <litus1001@gmail.com>
2021-03-31 23:48:11 +0300

    modularization

--- app.js
+++ app.js
@@ -4,24 +4,17 @@ const path = require('path');
 const AutoLoad = require('fastify-autoload');
 
 module.exports = async (fastify, opts) => {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
   // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: { opts }
   });
 
   // This loads all plugins defined in routes
-  // define your routes in one of these
+
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: { opts }
   });
+  fastify.register(require('fastify-http-client'));
 };
-// some code will be here soon...
-// and here...
