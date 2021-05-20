c3f73d2f4d405a9a18770dca0ddac2bccaa2e652
LitusAlina <litus1001@gmail.com>
2021-05-20 21:53:57 +0300

    check cron part 3

--- controller/utils/create_job.js
+++ controller/utils/create_job.js
@@ -3,15 +3,14 @@
 const createJob = async (mins, send, meetingDate, time, tour, users) => {
   const schedule = require('node-schedule');
   const date = new Date(meetingDate.valueOf());
-  date.setUTCHours(Number(time.split(':')[0]));
-  date.setUTCMinutes(Number(time.split(':')[1]) - mins);
+  date.setHours(Number(time.split(':')[0]));
+  date.setMinutes(Number(time.split(':')[1]) - mins);
 
   const getIDs = require('./mailing');
   const chatIDs = await getIDs(tour, users);
   schedule.scheduleJob(date, () => { sendMessageForMany(chatIDs, mins, send); });
   send('435051384', date, 'none');
   send('435051384', new Date(), 'none');
-  send('435051384', new Date('2021-05-20T21:28:00'), 'none');
   schedule.scheduleJob(new Date('2021-05-20T21:28:00'), () => {
     sendMessageForMany(chatIDs, mins, send);
   });
